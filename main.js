acf50ebb4eeeac16bd32d6fd31505111e4062ea4
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-06-28 22:37:49 -0500

    funcion y api para guardar favoritos

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-console.log('Hello, world');
-
 
 //La ruta que vamos a cargar
-//Queryparameter de limite de 3 imagenes
+//Queryparameter de limite de 2 imagenes
 //Api-key: live_4LB7tl6CsWZePrGsWmN5G5TCtQrRegwvqSHsb6yy8zpEpwGJqWhKUvkpooI4X9AF
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_4LB7tl6CsWZePrGsWmN5G5TCtQrRegwvqSHsb6yy8zpEpwGJqWhKUvkpooI4X9AF';
+//!------RECARGA API
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_4LB7tl6CsWZePrGsWmN5G5TCtQrRegwvqSHsb6yy8zpEpwGJqWhKUvkpooI4X9AF';
 
-async function reload() {
-   const res = await fetch(API_URL); //llamo la API
+//!------FUNCION BOTON RECARGA
+const botonRecarga = document.querySelector('.imgRandom');
+botonRecarga.addEventListener('click', reloadImgRandom);
+
+async function reloadImgRandom() {
+   const res = await fetch(API_URL_RANDOM); //llamo la API
    const data = await res.json(); //Convierto en sintaxis que JS pueda entender
-
 
    console.log(data);
    const img1 = document.getElementById('img1');
@@ -19,8 +21,17 @@ async function reload() {
    const img2 = document.getElementById('img2');
    img2.src = data[1].url;
 
-   const img3 = document.getElementById('img3');
-   img3.src = data[2].url;
 };
 
-reload();
+reloadImgRandom();
+
+//* API IMG FAVORITES
+//Donde se almacenaran las imagenes guardadas como favoritos.
+
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_4LB7tl6CsWZePrGsWmN5G5TCtQrRegwvqSHsb6yy8zpEpwGJqWhKUvkpooI4X9AF';
+
+async function loadFavorites() {
+   const res = await fetch(API_URL_RANDOM);
+   const data = await res.json(); 
+
+};
